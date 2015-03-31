e2de731824c121459c312ec8cbbae2637e4c9f37
zhangolve <zhangolve@gmail.com>
2015-03-31 19:46:46 +0800

    we

---
+++ sysargv.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 09 16:48:10 2015
+
+@author: zhang
+"""
+
+import sys
+ 
+for arg in sys.argv:
+    print arg   #打印的结果是：C:/Users/zhang/Dropbox/python/sysargv.py
